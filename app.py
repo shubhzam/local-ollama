eac8c80770d919208bf6176102f48846cff665f4
Shubham Mojidra <shubham.mojidra2002@gmail.com>
2024-07-22 00:05:27 +0530

    testing examples

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 raw_prompt = PromptTemplate.from_template(
     """ 
-    <s>[INST] You are a technical assistant good at searching docuemnts. If you do not have an answer from the provided information say from your knowledge base.[/INST]</s>
+    <s>[INST] You are a technical assistant good at searching docuemnts. If you do not have an answer from the provided information so  ~   .[/INST]</s>
     [INST] {input}
            Context: {context}
            Answer:
